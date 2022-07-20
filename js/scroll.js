a98c31581b48d5045d24884148bdd2eda362d1b5
battagliajose <86583581+battagliajose@users.noreply.github.com>
2022-07-20 00:32:04 -0300

    Finalizado Menu Navegacion

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,41 @@
-var menu_top = document.getElementById("menu_contacto");
-var menu_acerca = document.getElementById("menu_contacto");
-var menu_skills = document.getElementById("menu_contacto");
-var menu_formacion = document.getElementById("menu_contacto");
-var menu_proyectos = document.getElementById("menu_contacto");
+var menu_top = document.getElementById("menu_top");
+var menu_acerca = document.getElementById("menu_acerca");
+var menu_skills = document.getElementById("menu_skills");
+var menu_formacion = document.getElementById("menu_formacion");
+var menu_proyectos = document.getElementById("menu_proyectos");
 var menu_contacto = document.getElementById("menu_contacto");
 
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        left: 0,  
+        behavior: 'smooth'
+      });
+}
+function scrollToAcerca() {
+    scrollToClass(".acerca");
+}
+function scrollToSkills() {
+    scrollToClass(".skills");
+}
+function scrollToFormacion() {
+    scrollToClass(".formacion");
+}
+function scrollToProyectos() {
+    scrollToClass(".proyectos");
+}
 function scrollToContact() {
-    scrollTo(".contacto");
+    scrollToClass("#contacto");
 }
 
-function scrollTo(claseDestino) {
+function scrollToClass(claseDestino) {
     var scrollIntoView = document.querySelectorAll(claseDestino)[0];
     scrollIntoView.scrollIntoView({block: "end", behavior: "smooth"});
 }
 
+menu_top.addEventListener("click", scrollToTop);
+menu_acerca.addEventListener("click", scrollToAcerca);
+menu_skills.addEventListener("click", scrollToSkills);
+menu_formacion.addEventListener("click", scrollToFormacion);
+menu_proyectos.addEventListener("click", scrollToProyectos);
 menu_contacto.addEventListener("click", scrollToContact);
